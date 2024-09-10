ef3bb426db706bc6f886fc16690ab3fadbecea02
BenjaminCline17 <duckmancline@gmail.com>
2024-09-10 16:32:56 -0500

    Completed FizzBuzz

--- day-2/homework/fizz-buzz.js
+++ day-2/homework/fizz-buzz.js
@@ -10,11 +10,29 @@
  *  c. AND for the multiples of five print “Buzz”.
  *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
 */
+//I:Function takes no input
+//O:Print numbers from 1 to 100 with multiples of three printing "Fizz", multiples of five printing "Buzz", and multiples of both three and five printing "FizzBuzz"
+//C:N/A
+//E:N/A
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
 
-
-
+    //Use for loop to loop from 1 to 100
+    for (let i = 1; i <= 100; i++){
+        //if i is a multiple of 3 and not 5, print "Fizz"
+        if (i % 3 == 0 && i % 5 != 0){
+          console.log("Fizz");
+          //else if i is not a multiple of 3 and is a multiple of 5, print "Buzz"
+        } else if (i % 3 != 0 && i % 5 == 0){
+            console.log("Buzz");
+            //else if i is a multiple of 3 and 5, print "FizzBuzz"
+        } else if (i % 3 == 0 && i % 5 == 0){
+            console.log("FizzBuzz");
+            //else print i
+        } else {
+            console.log(i);
+        }
+    }
     // YOUR CODE GOES ABOVE HERE //
 }
 
