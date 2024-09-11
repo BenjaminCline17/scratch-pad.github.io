e815451c15e5a5cb298523ffe6f64afc45b27653
BenjaminCline17 <duckmancline@gmail.com>
2024-09-11 15:49:17 -0500

    Completed type.js

--- day-3/type.js
+++ day-3/type.js
@@ -102,12 +102,46 @@ function isObject(value) {
  * 
  * TIP: Similar to isObject, but we must return true if the value is an Array.
  */
+
+//I:Function takes in an input value
+//O:Function should return true if input value is an Array or an Object, otherwise return false
+//C:N/A
+//E:N/A
 function isCollection(value) {
     // YOUR CODE BELOW HERE //
     
+    //use if statement to determine if value is an array
+    if (Array.isArray(value) === true){
+        //if value is an array, return true
+        return true;
+    }
     
-    
-    
+    //use if statement to determine if value is null
+    if (value === null){
+        //if value is null, return false
+        return false;
+    } //use if statement to determine if value is an instance of Date
+    if (value instanceof Date){
+        //if value is an instance of Date, return false
+        return false;
+    }
+    //use if statement to determine if value is an object
+    if (typeof value === "object"){
+        //if value is an object, return true
+        return true;
+    }//Use if statement to determine if value is a string
+    if (typeof value === "string"){
+        //if value is a string, return false
+        return false;
+    } //use if statement to determine if value is a number
+    if (typeof value === "number"){
+        //if value is a number, return false
+        return false;
+    }//Use if statement to determine if value is a boolean
+    if (typeof value === "boolean"){
+        //if value is a boolean, return false
+        return false
+    }
     // YOUR CODE ABOVE HERE //
 }
 
@@ -129,12 +163,57 @@ function isCollection(value) {
  *    typeOf(134) -> "number"
  *    typeOf("javascript") -> "string"
  *    typeOf([1,2,3]) -> "array"
- */ 
+ */
+
+//I:Function takes in a input value
+//O:Function should return a string depending on the type of value
+//C:N/A
+//E:N/A
 function typeOf(value) {
     // YOUR CODE BELOW HERE //
     
-    
-    
+    //use if statement to check if value is an array
+    if (Array.isArray(value) === true){
+        //if value is an array, return "array"
+        return "array";
+    }
+    //use if statement to check if value is null
+    else if (value === null) {
+        //if value is null, return "null"
+        return "null";
+    }
+    //use if statement to check if value is an instance of Date
+    if (value instanceof Date) {
+        //if value is an instance of Date, return "date";
+        return "date";
+    }
+    //Use if statement to check if value is an object
+    if (typeof value === "object") {
+        //if value is an object, return "object"
+        return "object";
+    }
+    //Use if statement to check if value is a string
+    if (typeof value === "string") {
+        //if value is a string, return "string"
+        return "string";
+    } //Use if statement to determine if value is undefined
+    if (typeof value === "undefined"){
+        //if value is undefined return "undefined"
+        return "undefined";
+    }//Use if statement to determine if value is a number
+    if (typeof value === "number"){
+        //if value is a number return "number"
+        return "number";
+    }//Use if statement to determine if value is a boolean
+    if (typeof value === "boolean"){
+        //if value is a boolean return "boolean"
+        return "boolean";
+    }
+    //use if statement to determine if value is a function
+    if (typeof value === "function"){
+        //if value is a function, return "function"
+        return "function";
+    }
     
     // YOUR CODE ABOVE HERE //
 }
